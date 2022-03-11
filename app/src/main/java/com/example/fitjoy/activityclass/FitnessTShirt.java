9638a8711b3a8d21a74e4955950323c2790cf898
Tejash11 <tejashth1103@gmail.com>
2022-03-11 23:56:25 +0530

    added tshirt fragment

---
+++ app/src/main/java/com/example/fitjoy/activityclass/FitnessTShirt.java
@@ -0,0 +1,16 @@
+package com.example.fitjoy.activityclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fitjoy.R;
+
+public class FitnessTShirt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness_tshirt);
+    }
+}
